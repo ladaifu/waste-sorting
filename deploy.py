daf07091b685e991a225cb0d50a440a5bd8422d3
ryomado <ryomado97@gmail.com>
2019-11-09 02:25:35 +0700

    update deploy file

--- deploy.py
+++ deploy.py
@@ -1,14 +1,18 @@
 from fastai.vision import*
 import sys
-vision.defaults.device = vision.torch.device('cpu')
+import numpy as np
+import cv2
 
-path = vision.Path('.')
-learn = vision.load_learner(path)
-print(learn.data.batch_size)
+#vision.defaults.device = vision.torch.device('gpu')
+
+# path = vision.Path('exoprt80.pkl')
+# learn = vision.load_learner(path)
+learn = vision.load_learner('.', 'export80.pkl')
 
 imgpath = sys.argv[1]
 
 img = open_image(imgpath)
 
-pred = learn.predict(img)
-print(pred)
+# pred = learn.predict(img)
+pred_class,pred_idx,outputs = learn.predict(img)
+print(pred_class,pred_idx,outputs)
